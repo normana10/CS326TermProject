72a59ead47352ceffc095751d11a303b26709647
colinlee81 <colin.lee8581@gmail.com>
2017-11-30 14:39:26 -0500

    new gitignore, jquery bootstrap and datetimewidget added

--- PuppyCorps/PuppyCorps/settings.py
+++ PuppyCorps/PuppyCorps/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'PupsToPet.apps.PupstopetConfig',
+    'datetimewidget'
+    'jquery'
     #'django.contrib.easy_maps',
 ]
 
