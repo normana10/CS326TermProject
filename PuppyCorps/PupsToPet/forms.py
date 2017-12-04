8d04de63df721731ac214be4818a02e675a7d8e7
normana10 <arthurnormand@optonline.net>
2017-12-03 22:16:07 -0500

    Commented out broken class so I could run on heroku

--- PuppyCorps/PupsToPet/forms.py
+++ PuppyCorps/PupsToPet/forms.py
@@ -155,9 +155,9 @@ class UpdateProfileForm(forms.ModelForm):
         data = self.cleaned_data['email']
         return data
 
-    class Meta:
-        model = User
-        fields = ['first_name', 'last_name', 'email']
+    # class Meta:
+    #     model = User
+    #     fields = ['first_name', 'last_name', 'email']
 
 
 class UpdateProfileExtendedForm(forms.ModelForm):
